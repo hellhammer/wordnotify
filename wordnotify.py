79492877fcbe89add5c7037d09d004933a6892c7
hellhammer <mcontinium@gmail.com>
2014-07-21 12:36:53 +0300

    csv support

--- wordnotify.py
+++ wordnotify.py
@@ -1,12 +1,14 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import pynotify
 import argparse
 import random
 import time
+import csv
 import os
 
-mylist = []
+mydict = {}
+index = 0
 iconpath = os.getcwd()
 iconpath = os.path.join(iconpath, "wordnotify.png")
 parser = argparse.ArgumentParser()
@@ -14,31 +16,29 @@ parser.add_argument("filename")
 args = parser.parse_args()
 filename = args.filename
 
-with open(filename) as f:
-	for line in f:
-		mylist.append(line)
+with open(filename, 'rb') as f:
+    reader = csv.reader(f, delimiter=':', quoting=csv.QUOTE_NONE)
+    for line in reader:
+        index += 1
+        mydict[index] = line
 f.close()
 
-listsize = len(mylist)
+dictsize = len(mydict)
 
 pynotify.init("init")
 while True:
-	randnumber = random.randrange(0, listsize)
-	random.shuffle(mylist)
-	randline = mylist[randnumber]
-	print randnumber, randline
-	randdata = randline.split(":")
-	randword = randdata[0]
-	randdesc = randdata[1]
-	if (len(randdata) > 2):
-		randdesc = randdesc + "\n" + randdata[2]
-	randdesc = randdesc[:-1]
-	randdesc = "<b><i>" + randdesc + "</i></b>"
-	word = pynotify.Notification(randword, randdesc, iconpath)
-#	word.set_timeout(12)
-	word.show()
-	time.sleep(10)
+    randnumber = random.randrange(1, dictsize)
+#    random.shuffle(mydict)
+    line = mydict[randnumber]
+    if len(line) < 3:
+        eng, roma, jap = line[0], line[1], ''
+    else:
+        eng, roma, jap = line[0], line[1], line[2]
 
+    print '%i/%i: %s -- %s -- %s' % (randnumber, dictsize, eng, roma, jap)
 
-
-
+    desc = "<b><i>" + roma + "\n" + jap + "</i></b>"
+    word = pynotify.Notification(eng, desc, iconpath)
+    word.set_timeout(1000)
+    word.show()
+    time.sleep(2)
